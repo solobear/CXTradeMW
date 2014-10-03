9dc33fc9710134c1cd51e992f4f46fdc26fdec5b
solobear <magicstudio@MagicStudiomatoMac.local>
2014-10-03 16:46:08 +0800

    ios

---
+++ IOS/CXTradeMWClient/CXTradeMWReqCommand.h
@@ -0,0 +1,13 @@
+//
+//  CXTradeMWReqCommand.h
+//  CXTradeMWClient
+//
+//  Created by MagicStudio on 14-10-3.
+//  Copyright 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CXTradeMWReqCommand : NSObject
+
+@end
---
+++ IOS/CXTradeMWClient/CXTradeMWReqCommand.m
@@ -0,0 +1,23 @@
+//
+//  CXTradeMWReqCommand.m
+//  CXTradeMWClient
+//
+//  Created by MagicStudio on 14-10-3.
+//  Copyright 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CXTradeMWReqCommand.h"
+
+@implementation CXTradeMWReqCommand
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
